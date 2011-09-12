49b76409b2f06d9f5b62d8ce773a1b50d603313a
Xaymar <xaymar@levelnull.de>
2011-09-13 01:54:13 +0200

    Finished up the interface to MoneyDB. Will also now log Transactions :3 Moved util.java to mmoCore

--- src/main/java/mmo/Money/Account.java
+++ src/main/java/mmo/Money/Account.java
@@ -15,12 +15,17 @@ public class Account {
 	private MoneyDB account;
 	private Date lastModified;
 	private boolean changed;
+	private Money api;
 
-	public MoneyDB getAccount() {
+	public Account() {
+		api = new Money();
+	}
+
+	protected MoneyDB getAccount() {
 		return account;
 	}
 
-	public void setAccount(MoneyDB account) {
+	protected void setAccount(MoneyDB account) {
 		this.account = account;
 		this.setChanged(true);
 	}
@@ -29,26 +34,79 @@ public class Account {
 		return account.getOwner();
 	}
 
-	public void setOwner(String owner) {
+	protected void setOwner(String owner) {
 		account.setOwner(owner);
 	}
 
-	public long getMoneyAmount() {
+	protected long getMoneyAmount() {
 		return account.getAmount();
 	}
 
-	public void setMoneyAmount(long moneyAmount) {
+	protected void setMoneyAmount(long moneyAmount) {
 		account.setAmount(moneyAmount);
 	}
 
-	public boolean isChanged() {
+	protected boolean isChanged() {
 		return changed;
 	}
 
-	public void setChanged(boolean changed) {
+	protected void setChanged(boolean changed) {
 		this.changed = changed;
 		if (changed == true) {
 			this.lastModified.setTime(System.currentTimeMillis());
 		}
+	}
+
+	//Helpful functions
+	public long getMoney() {
+		return account.getAmount();
+	}
+
+	public TransactionDB setMoney(long money) {
+		TransactionDB tdb;
+		long total = money - account.getAmount();
+		if (total < 0) {
+			tdb = takeMoney(-total);
+		} else {
+			tdb = giveMoney(total);
+		}
+		tdb.setReason("setMoney");
+		return tdb;
+	}
+
+	public TransactionDB giveMoney(long money) {
+		TransactionDB tdb = api.getAccount("SERVER").transferMoney(this, money);
+		tdb.setReason("giveMoney");
+		return tdb;
+	}
+
+	public TransactionDB takeMoney(long money) {
+		TransactionDB tdb = transferMoney(api.getAccount("SERVER"), money);
+		tdb.setReason("takeMoney");
+		return tdb;
+	}
+
+	public TransactionDB transferMoney(Account to, long amount) {
+		TransactionDB tdb = createTransaction(this, to, amount, "transferMoney");
+		if (account.getAmount() >= amount) {
+			account.setAmount(account.getAmount() - amount);
+			to.getAccount().setAmount(to.getAccount().getAmount() + amount);
+			setChanged(true);
+			to.setChanged(true);
+		} else {
+			tdb.setFailed(true);
+		}
+		return tdb;
+	}
+
+	public TransactionDB createTransaction(Account from, Account to, long amount, String reason) {
+		TransactionDB tdb = new TransactionDB();
+		tdb.setFromAccount(from.getAccount());
+		tdb.setToAccount(to.getAccount());
+		tdb.setAmount(amount);
+		tdb.setReason(reason);
+		tdb.setOnDate(new Date());
+		tdb.setFailed(false);
+		return tdb;
 	}
 }
